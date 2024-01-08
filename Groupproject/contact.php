5a71b463194b2dd5e12927524d88b17c30e93362
GohYongZhen <128480796+GohYongZhen@users.noreply.github.com>
2024-01-08 16:41:58 +0800

    Update contact.php

--- Groupproject/contact.php
+++ Groupproject/contact.php
@@ -21,6 +21,8 @@ body{
 </head>
 <body>
     <?php include('header.php');?>
+
+<form method="POST" action="contact_action.php">
     <div class="contact-form">
         <h1>Contact Us</h1>
         <div class="container">
@@ -28,11 +30,11 @@ body{
                 <div class="content">
                     <h2>Contact Us</h2>
                     <form action="#" method="post">
-                        <input type="text" name="name" placeholder="Enter Your Name">
+                        <input type="text" id="name" name="name" placeholder="Enter Your Name" required>
                       
-                        <input type="email" name="name" placeholder="Enter Your Email">
-                        <textarea name="message" placeholder="Your Message"></textarea>                   
-             <button type="submit" class="btn">Send <i class="fas fa-paper-plane"></i></button>
+                        <input type="email" id="email" name="email" placeholder="Enter Your Email" required>
+                        <textarea type="text" id="message" name="message" placeholder="Your Message"></textarea>                   
+             <button type="submit" name="submit" class="btn">Send<i class="fas fa-paper-plane"></i></button>
                     </form>
                 </div>
                 <div class="form-img">
@@ -41,29 +43,39 @@ body{
             </div>
         </div>
     </div> 
+
+</form>
+    
     <!--trying to put contact and icons-->
+    <?php
+        $sql = "SELECT * FROM footer";
+        $result = mysqli_query($conn,$sql);
+
+    while($data=mysqli_fetch_array($result)){
+        ?>
     <section class="contact-card">
         <div class="contact-info" style="color:black">
             <div class="card">
                 <i class="card-icon far fa-envelope"></i>
-                <p>pre-school@example.com</p>
+                <p><?php echo $data['email']?></p>
             </div>
         </div>
 
         <div class="contact-info" style="color:black">
             <div class="card">
                 <i class="card-icon fa-solid fa-phone"></i>
-                <p>+6078828576</p>
+                <p><?php echo $data['phoneNumber']?></p>
             </div>
         </div>        
 
         <div class="contact-info" style="color:black">
             <div class="card">
                 <i class="card-icon fa fa-map-marker"></i>
-                <p>Kuala Lumpur Malaysia</p>
+                <p><?php echo $data['address']?></p>
             </div>
         </div>          
     </section>
+    <?php } ?>
 
 
     <?php include('footer.php');?>
