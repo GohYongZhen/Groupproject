1c12c5aa2c41231f1f7220a6d5ad7ae4dd9a8d3e
TanJinHang <155894730+TanJinHang@users.noreply.github.com>
2024-01-10 07:15:26 +0800

    Add files via upload

---
+++ Groupproject/enrollment/enrollment_edit.php
@@ -0,0 +1,329 @@
+<?php
+session_start();
+//database connection
+include("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>UPDATE JA Form</title>
+    <link rel="stylesheet" href="css/application_admin_header.css">
+
+</head>
+<body>
+   
+    
+    <h2>Edit INFO</h2> 
+    <?php 
+            $id=null;
+				$father_name="";
+				$mother_name="";
+				$parent_contact="";
+				$child_name="";
+				$age="";
+				$program="";
+				$message="";
+				$child_birthday="";
+				$time="";
+				$photo="";
+				$status="";
+
+            if(isset($_GET["id"]) && $_GET["id"] != ""){ ; 
+                $sql = "SELECT * FROM enrollment WHERE enrol_id=" . $_GET["id"];
+
+                $result = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_assoc($result);
+                    $id = $row["enrol_id"];
+					$father_name = $row["father_name"];
+					$mother_name = $row["mother_name"];
+					$parent_contact = $row["parent_contact"];
+					$parent_email = $row["parent_email"];
+					$child_name = $row["child_name"];
+					$age = $row["age"];
+					$program = $row["program"];
+					$message = $row["message"];
+					$child_birthday = $row["child_birthday"];
+					$time=$row["enrol_time"];
+					$photo=$row["child_photo"];
+					$status=$row["enrol_status"];
+					
+            
+                }  
+           }  
+            
+        ?> 
+
+<form style="padding:0 10px;" method="POST" action="enrollment_edit_action.php" enctype="multipart/form-data"  onsubmit="return validateForm()" id="myForm">
+                <!--hidden value: id to be submitted to action page--> 
+                <div class="form-container">
+                <input type="text" id="id" name="id" value="<?=$_GET['id']?>"hidden>
+                
+                    <tr>
+                        <th>FATHER NAME</th>
+                                <td> 
+                                <?php 
+                            if($father_name!=""){ 
+                                echo '<input type="text" name="father_name" size="5" value="' . $father_name . '" required>'; 
+                            } 
+                            else { 
+                            ?> 
+                                <input type="text" name="father_name" size="5" required> 
+                            <?php 
+                                } 
+                            ?> 
+                        </td> 
+                        </br>
+                    </tr>
+
+                    <tr>
+                        <th>MOTHER NAME</th>
+                                <td> 
+                                <?php 
+                            if($mother_name!=""){ 
+                                echo '<input type="text" name="mother_name" size="5" value="' . $mother_name . '" required>'; 
+                            } 
+                            else { 
+                            ?> 
+                                <input type="text" name="mother_name" size="5" required> 
+                            <?php 
+                                } 
+                            ?> 
+                        </td> 
+                        </br>
+                    </tr>
+
+
+                    <tr>
+                        <th>CONTACT</th>
+                                <td> 
+                                <?php 
+                            if($parent_contact!=""){ 
+                                echo '<input type="text" name="parent_contact" size="5" value="' . $parent_contact . '" required>'; 
+                            } 
+                            else { 
+                            ?> 
+                                <input type="text" name="parent_contact" size="5" required> 
+                            <?php 
+                                } 
+                            ?> 
+                        </td> 
+                        </br>
+                    </tr>
+
+
+                    <tr>
+                        <th>EMAIL</th>
+                                <td> 
+                                <?php 
+                            if($parent_email!=""){ 
+                                echo '<input type="text" name="parent_email" size="5" value="' . $parent_email . '" required>'; 
+                            } 
+                            else { 
+                            ?> 
+                                <input type="text" name="parent_email" size="5" required> 
+                            <?php 
+                                } 
+                            ?> 
+                        </td> 
+                        </br>
+                    </tr>
+
+                    <tr>
+                        <th>CHILDREN NAME</th>
+                                <td> 
+                                <?php 
+                            if($child_name!=""){ 
+                                echo '<input type="text" name="child_name" size="5" value="' . $child_name . '" required>'; 
+                            } 
+                            else { 
+                            ?> 
+                                <input type="text" name="child_name" size="5" required> 
+                            <?php 
+                                } 
+                            ?> 
+                        </td> 
+                        </br>
+                    </tr>
+
+                    <tr>
+                        <th>PROGRAM</th>
+                                <td> 
+                                <select size="1" name="program" required> 
+                        <option value=""><?php echo $program;?></option> 
+                        <?php 
+                                    if($program=="PlayGroup-1.8 to 3 years") 
+                                        echo '<option value="PlayGroup-1.8 to 3 years" selected>PlayGroup-1.8 to 3 years</option>'; 
+                                    else 
+                                        echo '<option value="PlayGroup-1.8 to 3 years">PlayGroup-1.8 to 3 years</option>';
+
+                                    if($program=="Nursery-2.5 to 4 years") 
+                                        echo '<option value="Nursery-2.5 to 4 years" selected>Nursery-2.5 to 4 years</option>'; 
+                                    else 
+                                        echo '<option value="Nursery-2.5 to 4 years">Nursery-2.5 to 4 years</option>';
+
+                                        if($program=="Junior KG- 3.5 to 5 years") 
+                                        echo '<option value="Junior KG- 3.5 to 5 years" selected>Junior KG- 3.5 to 5 years</option>'; 
+                                    else 
+                                        echo '<option value="Junior KG- 3.5 to 5 years">Junior KG- 3.5 to 5 years</option>';
+
+                                        if($program=="Senior KG- 4.5 to 6 years") 
+                                        echo '<option value="Senior KG- 4.5 to 6 years" selected>Senior KG- 4.5 to 6 years</option>'; 
+                                    else 
+                                        echo '<option value="Senior KG- 4.5 to 6 years">Senior KG- 4.5 to 6 years</option>';
+
+
+                                    
+
+                            ?> 
+		                        </select> 
+                        </td> 
+                        </br>
+                    </tr>
+
+                    <tr>
+                        <th>MESSAGE</th> 
+                        </br>
+                        <td>
+                           
+                        <textarea rows="4" name="message" cols="20" rows="7"><?php echo $message;?></textarea> 
+                        </td> 
+                        </br>
+				</tr> 
+
+                    <tr>
+                        <th>AGE</th>
+                       <td> 
+                                <select size="1" name="age" required> 
+                        <option value=""><?php echo $age;?></option> 
+                                <?php 
+                            if($age=="18 Month-3 Year") 
+                                        echo '<option value="18 Month-3 Year" selected>18 Month-3 Year</option>'; 
+                                    else 
+                                        echo '<option value="18 Month-3 Year">18 Month-3 Year</option>';
+                                    
+                            if($age=="2-3 Year") 
+                                        echo '<option value="2-3 Year" selected>2-3 Year</option>'; 
+                                    else 
+                                        echo '<option value="2-3 Year">2-3 Year</option>';
+
+                             if($age=="3-4 Year") 
+                                        echo '<option value="3-4 Year" selected>3-4 Year</option>'; 
+                                    else 
+                                        echo '<option value="3-4 Year">3-4 Year</option>';
+
+                            if($age=="4-5 Year") 
+                                        echo '<option value="4-5 Year" selected>4-5 Year</option>'; 
+                                    else 
+                                        echo '<option value="4-5 Year">4-5 Year</option>';
+
+                             if($age=="5-6 Year") 
+                                        echo '<option value="5-6 Year" selected>5-6 Year</option>'; 
+                                    else 
+                                        echo '<option value="5-6 Year">5-6 Year</option>';
+
+
+                            ?> 
+                            </select>
+                        </td> 
+                        
+                    </tr>
+
+                    <br></br>
+                      
+
+
+
+                    <tr>
+                        <th>BIRTHDAY</th>
+                                <td> 
+                                <input type="date" name="child_birthday"  value="<?php echo $child_birthday;?>">
+                        </td> 
+                        </br>
+                    </tr>
+
+
+
+
+                    <tr>
+                        <th>UPLOADED PHOTO</th>
+                        <td>:</td> 
+                            <td>
+                            <input type="text" disabled value="<?=$photo;?>">
+                            </td> 
+                        </br>
+                    </tr>
+
+                    <tr>
+                        <th>NEW PHOTO TO UPLOAD</th>
+                        <td>:</td> 
+                        <input type="file" name="file" id="file" >
+                            <br>
+                            
+                        </br>
+                        
+                    </tr>
+
+                    <tr>
+                            <button type="submit" value="Submit" class="btn">Update </button>  
+                            
+                            <button type="reset" value="Reset" class="btn" onclick="resetForm()" >Reset </button>  
+                           
+                            <button type="button" value="Clear" class="btn" onclick="resetForm()">Clear </button>  
+                           
+                            <button type="button" class="btn" onclick="goBack()">Go Back</button>
+
+                    </tr>
+
+               
+            </div>
+    </form>
+
+             <script>
+        //reset form after modification to a php echo to fields//
+
+        function resetForm() { 
+            document.getElementById("myForm").reset(); 
+            } 
+            
+            //this clear form to empty the form for new data 
+         function clearForm() { 
+            var form = document.getElementById("myForm"); 
+            
+            if (form) { 
+            var inputs = form.getElementsByTagName("input"); 
+            var textareas = form.getElementsByTagName("textarea"); 
+            
+                //clear select 
+            form.getElementsByTagName("select")[0].selectedIndex=0; 
+                
+            //clear all inputs 
+                for (var i = 0; i < inputs.length; i++) { 
+                if (inputs[i].type !== "button" && inputs[i].type !== "submit" && inputs[i].type !== "reset") {
+                inputs[i].value = ""; 
+                } 
+            } 
+            
+            //clear all textareas 
+            for (var i = 0; i < textareas.length; i++) {
+            textareas[i].value = ""; 
+            } 
+            } else { 
+            console.error("Form not found"); 
+            } 
+            } 
+
+            function goBack() {
+                // Use the history object to navigate back
+                window.history.back();
+                }
+    </script>
+</body>
+</html>
+
+
+                    
